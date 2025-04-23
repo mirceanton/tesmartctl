1d20c1c53faf628d1149da86be9e7f3d67e23062
Mircea-Pavel Anton <contact@mirceanton.com>
2025-04-23 19:20:10 +0300

    feat: config file and handling subcommands (#1)

---
+++ cmd/config.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// configCmd represents the config command
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Manage configuration settings",
+	Long:  `View or modify configuration settings for the KVM connection.`,
+}
+
+// getConfigCmd represents the config get subcommand
+var getConfigCmd = &cobra.Command{
+	Use:   "get",
+	Short: "View current configuration",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Current configuration:\n")
+		fmt.Printf("  IP Address: %s\n", viper.GetString("ip_address"))
+		fmt.Printf("  Port: %s\n", viper.GetString("port"))
+		fmt.Printf("  Number of inputs: %d\n", viper.GetInt("num_inputs"))
+		fmt.Printf("\nConfiguration file: %s\n", viper.ConfigFileUsed())
+	},
+}
+
+// setConfigCmd represents the config set subcommand
+var setConfigCmd = &cobra.Command{
+	Use:   "set [ip|port|inputs] [value]",
+	Short: "Modify configuration",
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		param := args[0]
+		value := args[1]
+
+		switch param {
+		case "ip":
+			viper.Set("ip_address", value)
+			fmt.Printf("IP address set to: %s\n", value)
+		case "port":
+			viper.Set("port", value)
+			fmt.Printf("Port set to: %s\n", value)
+		case "inputs":
+			numInputs, err := strconv.Atoi(value)
+			if err != nil || numInputs < 1 {
+				return fmt.Errorf("number of inputs must be a positive integer")
+			}
+			viper.Set("num_inputs", numInputs)
+			fmt.Printf("Number of inputs set to: %d\n", numInputs)
+		default:
+			return fmt.Errorf("unknown parameter: %s", param)
+		}
+
+		if err := viper.WriteConfig(); err != nil {
+			return fmt.Errorf("error writing config: %v", err)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	configCmd.AddCommand(getConfigCmd)
+	configCmd.AddCommand(setConfigCmd)
+
+	rootCmd.AddCommand(configCmd)
+}
---
+++ cmd/root.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile string
+	debug   bool
+
+	rootCmd = &cobra.Command{
+		Use:   "tesmartctl",
+		Short: "Control a TeSmart KVM switch",
+	}
+)
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Global flags
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/tesmartctl.yaml)")
+	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "enable debug output")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Check if .config directory exists and create it if not
+		configDir := filepath.Join(home, ".config")
+		if err := os.MkdirAll(configDir, 0755); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name "tesmartctl.yaml"
+		viper.AddConfigPath(configDir)
+		viper.SetConfigName("tesmartctl")
+		viper.SetConfigType("yaml")
+	}
+
+	// Set default values
+	viper.SetDefault("ip_address", "192.168.0.10")
+	viper.SetDefault("port", "5000")
+	viper.SetDefault("num_inputs", 8)
+
+	// Read in environment variables that match
+	viper.AutomaticEnv()
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		// Config file found
+		if debug {
+			fmt.Println("Using config file:", viper.ConfigFileUsed())
+		}
+	} else {
+		// Config file not found, creating one with defaults
+		if err := viper.SafeWriteConfig(); err != nil {
+			fmt.Println("Failed to create config file:", err)
+		} else if debug {
+			fmt.Println("Created new config file:", viper.ConfigFileUsed())
+		}
+	}
+}
